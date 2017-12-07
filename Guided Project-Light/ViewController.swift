cab694bc6ee3b4f556a4a05c416ae15461967c0a
student <student@productservice.ttknn.net>
2017-12-07 14:14:08 +0300

    Initial Commit

---
+++ Guided Project-Light/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Guided Project-Light
+//
+//  Created by student on 07.12.2017.
+//  Copyright © 2017 student. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
