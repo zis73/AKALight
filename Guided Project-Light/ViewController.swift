458452d58737829737d4db7818dd9c516b5e3538
student <student@productservice.ttknn.net>
2017-12-07 14:48:17 +0300

    Добавил код

--- Guided Project-Light/ViewController.swift
+++ Guided Project-Light/ViewController.swift
@@ -9,7 +9,32 @@
 import UIKit
 
 class ViewController: UIViewController {
+   var x = 0
+    @IBAction func button(_ sender: Any) {
+        
+        x += 1
+        if x > 4{
+            x=0
+        }
 
+        next()
+    }
+    
+    
+    func next() {
+        switch x {
+        case 1:
+            view.backgroundColor = .yellow
+        case 2:
+            view.backgroundColor = .green
+        case 3:
+            view.backgroundColor = .red
+        case 4:
+            view.backgroundColor = .blue
+        default:
+            view.backgroundColor = .white
+        }
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
